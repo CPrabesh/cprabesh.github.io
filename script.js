3bbf8ee0eed3efbe7fc96245944b5f3958944333
CPrabesh <chhetriprabesh5@gmail.com>
2026-05-03 20:53:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<!-- Mobile Menu Navigation onClick function !-->
+
+  function toggleMenu() {
+  // Finds the menu and toggles the "active" class on or off
+  var menu = document.getElementById("nav-menu");
+  menu.classList.toggle("active");
+}
+
